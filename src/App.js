290eac3296fb68ad074da15b8b50034115e78f23
tika.kristiani <tika.kristiani@gmail.com>
2022-01-26 09:38:51 +0700

    able to understand jsx

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Jeremy'
+  function getButtonText() {
+    return "Click Me"
+  }
+  const buttonName = {text: "Click Me"}
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome {name}!</h1>
+        <label htmlFor="name">Enter Name</label>
+        <input id='name' type="text"/>
+        <button>{getButtonText()}</button>
+        <button>{buttonName.text}</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
